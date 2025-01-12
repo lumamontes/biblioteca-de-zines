bdec07249716ad6e0e6d19bdca7f422ad811a5ef
lumamontes <lumagoesmontes@gmail.com>
2025-01-12 04:15:15 -0300

    feat: add new gif to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default async function HomePage() {
       </div>
       <div className="h-48 w-full  md:w-1/2 flex items-center justify-center">
         <Image
-          src="/cover.png"
+          src="/ezgif-5-847f1efa8d.gif"
           alt="Logo da biblioteca de zines"
           width={301}
           height={288}
