9619acb3441803958df085c75b9e891abae22185
lumamontes <lumagoesmontes@gmail.com>
2025-01-12 04:21:01 -0300

    fix: broken build

--- src/app/zines/layout.tsx
+++ src/app/zines/layout.tsx
@@ -1,9 +1,8 @@
 // layout.tsx
 import Header from "@/components/header";
 import NavSocialLinks from "@/components/nav-social-links";
-import { ScriptProps } from "next/script";
 
-export default function ZineLayout({ children }: ScriptProps) {
+export default function ZineLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen  max-w-6xl mx-auto md:px-12 flex-col">
       <Header />
