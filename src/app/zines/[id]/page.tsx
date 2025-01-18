dd348920819250685923c5f2129b0c7ab9543e2e
lumamontes <lumagoesmontes@gmail.com>
2025-01-17 23:20:49 -0300

    feat: add custom metadata per zine and update og image

--- src/app/zines/[id]/page.tsx
+++ src/app/zines/[id]/page.tsx
@@ -2,6 +2,42 @@ import PDFViewer from "@/components/pdf-viewer";
 import { getZineByUuid } from "@/services/zine-service";
 import { getPreviewUrl } from "@/utils/assets";
 
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const preview = await getZineByUuid(id);
+
+  if (!preview) {
+    return {
+      title: "Zine não encontrada!",
+      description: "A zine que você está procurando não foi encontrada.",
+    };
+  }
+
+  return {
+    title: `${preview.title} por ${preview.author_name}`,
+    description: preview.description,
+    openGraph: {
+      title: preview.title,
+      description: preview.description,
+      images: [
+        {
+          url: preview.cover_image,
+          width: 1200,
+          height: 630,
+        },
+      ],
+      url: `https://biblioteca-de-zines.vercel.app/zine/${id}`,
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: preview.title,
+      description: preview.description,
+      images: [preview.cover_image],
+    },
+  };
+}
+
 export default async function ZinePreview({
   params,
 }: {
