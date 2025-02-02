14a07597cd2d1f379fba49bb0a7d7c889639e889
lumamontes <lumagoesmontes@gmail.com>
2025-02-02 03:53:21 -0300

    add full text search and filter to zines page

---
+++ src/components/zines/filtered-zines.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import ZineCard from "@/components/zine-card";
+import { searchZines } from "@/services/zine-service";
+import { debounce } from "lodash";
+import { Zine } from "@/@types/zine";
+
+export default function FilteredZines({ initialZines }: { initialZines: Zine[] }) {
+  const [zines, setZines] = useState(initialZines);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filter, setFilter] = useState("all"); // "all" | "recent"
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchZines = async () => {
+      setLoading(true);
+      console.log("searchQuery", searchQuery);
+      const zines = await searchZines({ search: searchQuery, orderBy: filter });
+      setZines(zines);
+      setLoading(false);
+    };
+
+    fetchZines();
+  }, [searchQuery, filter]);
+
+  const handleSearch = debounce((searchQuery: string) => {
+    setSearchQuery(searchQuery);
+  }, 600);
+
+  return (
+    <div>
+      {/* Filter Buttons */}
+      <div className="flex flex-wrap gap-4 mb-4">
+  <button
+    className={`px-6 py-2.5 rounded-lg font-semibold transition-all duration-300
+      ${filter === "all" 
+        ? "bg-blue-600 text-white shadow-md hover:bg-blue-700 hover:shadow-lg" 
+        : "bg-gray-200 text-gray-800 hover:bg-gray-300 hover:text-gray-900"}`}
+    onClick={() => setFilter("all")}
+  >
+    Todas
+  </button>
+  
+  <button
+    className={`px-6 py-2.5 rounded-lg font-semibold transition-all duration-300
+      ${filter === "recent" 
+        ? "bg-blue-600 text-white shadow-md hover:bg-blue-700 hover:shadow-lg" 
+        : "bg-gray-200 text-gray-800 hover:bg-gray-300 hover:text-gray-900"}`}
+    onClick={() => setFilter("recent")}
+  >
+    Mais recentes
+  </button>
+</div>
+
+      <div className="flex gap-4 mb-4 w-full">
+        <input 
+          type="text" 
+          placeholder="Buscar zine ou autor" 
+          className="border border-gray-300 rounded-md px-3 py-2 focus:ring-2 focus:ring-indigo-500 w-full"
+          onChange={(event) => handleSearch(event.target.value)}
+        />
+      </div>
+
+      <div 
+      role="grid"
+      className="grid grid-cols-1 gap-6 px-4 py-8 sm:px-6 md:grid-cols-2 lg:grid-cols-3"
+    >
+      {/* Zine Cards */}
+      {loading ? (
+        <p role="status">Carregando zines...</p>
+      ) : zines.length === 0 ? (
+        <p role="status">Nenhuma zine encontrada.</p>
+      ) : (
+        zines.map((zine) => <ZineCard zine={zine} key={zine.uuid} />)
+      )}
+      </div>
+    </div>
+  );
+}
--- src/services/zine-service.ts
+++ src/services/zine-service.ts
@@ -1,6 +1,7 @@
 import { Zine } from "@/@types/zine";
 import { createClient } from "@/utils/supabase/server";
-import { PostgrestResponse } from "@supabase/supabase-js";
+import { createClient as createBrowser } from "@/utils/supabase/client";
+import { PostgrestResponse, PostgrestSingleResponse } from "@supabase/supabase-js";
 
 export const getPublishedZines = async (): Promise<Zine[]> => {
   const supabase = await createClient();
@@ -56,4 +57,83 @@ export const getZineBySlug = async (slug: string): Promise<Zine | null> => {
 
   return data ?? null;
 
+};
+
+type SearchZineProps = {
+  search?: string | null;
+  orderBy?: string;
+  authorId?: number | null;
+};
+
+export const searchZines = async ({
+  search = null,
+  orderBy = "all",
+}): Promise<Zine[]> => {
+  const supabase = await createBrowser();
+
+  let query = supabase
+    .from("library_zines")
+    .select(
+      "*, library_zines_authors!inner(authors!inner(id, name, url))"
+    );
+
+  if (search) {
+    query = query.textSearch("title_fts", search, {
+      config: "portuguese",
+      type: "websearch",
+    });
+  }
+
+  if (orderBy === "recent") {
+    query = query.order("created_at", { ascending: false });
+  }
+
+  const { data: zines, error } = await query;
+
+  if (error) {
+    console.error("Error fetching zines:", error.message);
+    throw new Error(error.message);
+  }
+
+  const authorQuery = await supabase
+    .from("authors")
+    .select("id, name")
+    .textSearch("name_fts", search? search : '', {
+      config: "portuguese",
+      type: "websearch",
+    });
+
+  if (authorQuery.error) {
+    console.error("Error fetching authors:", authorQuery.error.message);
+    throw new Error(authorQuery.error.message);
+  }
+
+  const authorIds = authorQuery.data.map((author) => author.id);
+
+  if (authorIds.length > 0) {
+    let queryAuthorZines = supabase
+      .from("library_zines")
+      .select("*, library_zines_authors!inner(authors!inner(id, name, url))")
+      .in("library_zines_authors.authors.id", authorIds);
+
+    if (orderBy === "recent") {
+      queryAuthorZines = queryAuthorZines.order("created_at", { ascending: false });
+    }
+
+    const { data: authorZinesData, error: authorZinesError } = await queryAuthorZines;
+
+    if (authorZinesError) {
+      console.error("Error fetching zines by author:", authorZinesError.message);
+      throw new Error(authorZinesError.message);
+    }
+
+    const mergedZines = [...zines, ...authorZinesData].filter(
+      (value, index, self) =>
+        index === self.findIndex((t) => t.id === value.id)
+    );
+
+    return mergedZines;
+  }
+
+  return zines ?? [];
 };
